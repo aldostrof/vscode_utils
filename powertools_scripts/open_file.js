613c6a1add6b422e3089816c77968ff8e91817dc
Aldo Strofaldi <aldo.strofaldi@gmail.com>
2020-09-09 13:05:49 +0200

    Update docs in open_file.js

--- powertools_scripts/open_file.js
+++ powertools_scripts/open_file.js
@@ -2,12 +2,15 @@ exports.execute = async (args) => {
     // args => https://egodigital.github.io/vscode-powertools/api/interfaces/_contracts_.workspacecommandscriptarguments.html
 
     // s. https://code.visualstudio.com/api/references/vscode-api
+    
+    // Opens a specific file whose path is fetched from settings.json.
     const vscode = args.require('vscode');
     const path = require('path');
 
     let rootPath = vscode.workspace.rootPath;
-    // read the file path from settings.json
+    // read the file path from settings.json, using property: my.file.path (or use your own and change accordingly)
     let filePath = vscode.workspace.getConfiguration('my.file').get('path');
+    // specify your file name here
     let fileName = "your_file_name";
     let fileFull = path.join(filePath, fileName);
     var openPath = vscode.Uri.parse("file:///" + fileFull);
